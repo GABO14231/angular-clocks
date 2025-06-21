80f1e09a7f35d75590da662040083d7e0744ad53
GABO1423 <176758048+gabo14231@users.noreply.github.com>
2025-06-20 21:52:08 -0400

    Frontend: Add AuthGuard and LoginGuard
    - AuthGuard: Used to verify the authentication process.
    - LoginGuard: Makes sure we are logged in before allowing access to certain pages.

---
+++ frontend/src/app/guards/LoginGuard.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, Router, UrlTree} from '@angular/router';
+import {AuthService} from '../services/AuthService';
+import {Observable} from 'rxjs';
+import {filter, map, take, switchMap} from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class LoginGuard implements CanActivate
+{
+    constructor(private auth: AuthService, private router: Router) {}
+
+    canActivate(): Observable<boolean | UrlTree>
+    {
+        return this.auth.authInitialized$.pipe(filter(initialized => initialized === true),
+            take(1), switchMap(() => this.auth.user$), take(1),
+            map(user => {return !user ? true : this.router.createUrlTree(['/']);}));
+    }
+}
