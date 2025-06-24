1d3a824e7260a898778cc29db31f39ec79cc0aca
GABO1423 <176758048+gabo14231@users.noreply.github.com>
2025-06-24 19:00:01 -0400

    Frontend: Add ClockMenu Component
    Will be used to choose from the 10 time visualizers.

---
+++ frontend/src/app/components/ClockMenu/ClockMenu.ts
@@ -0,0 +1,19 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+export interface MenuOption {label: string; action: () => void;}
+
+@Component({selector: 'app-menu', standalone: true, imports: [CommonModule, FormsModule],
+    templateUrl: './ClockMenu.html',styleUrls: ['./ClockMenu.css']})
+
+export class ClockMenu
+{
+    @Input() options: MenuOption[] = [];
+    @Output() optionSelected: EventEmitter<MenuOption> = new EventEmitter<MenuOption>();
+
+    onOptionSelect(option: MenuOption): void
+    {
+        if (option.action) option.action();
+        this.optionSelected.emit(option);
+    }
+}
--- frontend/src/app/pages/ClockDashboard/ClockDashboard.ts
+++ frontend/src/app/pages/ClockDashboard/ClockDashboard.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {NavbarComponent} from '../../components/Navbar/Navbar';
 import {TimeControlsComponent} from '../../components/TimeControls/TimeControls';
+import {ClockMenu, MenuOption} from '../../components/ClockMenu/ClockMenu';
 import {AuthService} from '../../services/AuthService';
 import {Subscription} from 'rxjs';
 
@@ -11,7 +12,7 @@ export interface NavbarOption
     method?: () => void;
 }
 
-@Component({selector: 'app-dashboard', standalone: true, imports: [NavbarComponent, TimeControlsComponent],
+@Component({selector: 'app-dashboard', standalone: true, imports: [NavbarComponent, TimeControlsComponent, ClockMenu],
     templateUrl: './ClockDashboard.html', styleUrls: ['./ClockDashboard.css']})
 
 export class ClockDashboardPage implements OnInit, OnDestroy
@@ -20,11 +21,14 @@ export class ClockDashboardPage implements OnInit, OnDestroy
     dashboardOptions = [{label: "Home", path: "/"}, {label: "Settings", path: "/profile"},
         {label: "Logout", path: "/", method: () => this.handleLogout()}];
     userSub!: Subscription;
+    menuOptions = [{label: 'Option 1', action: () => console.log("Action for Option 1 executed")},
+        {label: 'Option 2', action: () => console.log("Action for Option 2 executed")}];
 
     constructor(private authService: AuthService) {}
 
     ngOnInit(): void {this.userSub = this.authService.user$.subscribe(u => this.user = u);}
     ngOnDestroy(): void {if (this.userSub) this.userSub.unsubscribe();}
+    handleMenuSelection(option: MenuOption) {console.log('User selected', option.label);}
 
     handleLogout(): void
     {
