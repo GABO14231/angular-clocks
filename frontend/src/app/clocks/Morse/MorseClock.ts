3e49891a5825dd6cb4bd694740a34eb5cb5df101
GABO1423 <176758048+gabo14231@users.noreply.github.com>
2025-06-26 19:52:22 -0400

    Frontend: Improve Morse Clock
    Now seconds are shown in Morse text, makes them much more readable.

--- frontend/src/app/clocks/Morse/MorseClock.ts
+++ frontend/src/app/clocks/Morse/MorseClock.ts
@@ -12,21 +12,19 @@ export class MorseClockComponent implements OnDestroy
     private _second = '00';
     private _period = '';
 
-    @Input() set hour(v: string) { if (v !== this._hour) { this._hour = v; this.resetHour(); } }
-    @Input() set minute(v: string) { if (v !== this._minute) { this._minute = v; this.resetMinute(); } }
-    @Input() set second(v: string) { if (v !== this._second) { this._second = v; this.resetSecond(); } }
-    @Input() set period(v: string) { if (v !== this._period) { this._period = v; this.resetHour(); } }
+    @Input() set hour(v: string) {if (v !== this._hour) {this._hour = v; this.resetHour();}}
+    @Input() set minute(v: string) {if (v !== this._minute) {this._minute = v; this.resetMinute();}}
+    @Input() set second(v: string) {if (v !== this._second) {this._second = v; this.updateSeconds();}}
+    @Input() set period(v: string) {if (v !== this._period) {this._period = v; this.resetHour();}}
 
     blinkH = false;
     blinkM = false;
-    blinkS = false;
+    morseS = '';
     private timerH: any;
     private timerM: any;
-    private timerS: any;
 
     private T_H: MorseTiming = {dot: 200, dash: 600, intra: 200, letter: 600, loop: 100};
     private T_M: MorseTiming = {dot: 150, dash: 450, intra: 150, letter: 450, loop: 50};
-    private T_S: MorseTiming = {dot: 60, dash: 180, intra: 60, letter: 180, loop: 20};
 
     private readonly MORSE: Record<string, string> =
     {
@@ -40,7 +38,7 @@ export class MorseClockComponent implements OnDestroy
         '7': '--...',
         '8': '---..',
         '9': '----.'
-      };
+    };
 
     constructor(private zone: NgZone) {}
 
@@ -48,25 +46,34 @@ export class MorseClockComponent implements OnDestroy
     {
         clearTimeout(this.timerH);
         clearTimeout(this.timerM);
-        clearTimeout(this.timerS);
     }
 
     private resetHour() {this.buildAndPlay(this._hour, this.blinkHSetter, this.timerSetterH, this.T_H);}
     private resetMinute() {this.buildAndPlay(this._minute, this.blinkMSetter, this.timerSetterM, this.T_M);}
-    private resetSecond() {this.buildAndPlay(this._second, this.blinkSSetter, this.timerSetterS, this.T_S);}
+
+    private updateSeconds()
+    {
+        const s = parseInt(this._second, 10) || 0;
+        const sStr = String(s).padStart(2, '0');
+        let morseCode = '';
+        sStr.split('').forEach((d, idx) =>
+        {
+            morseCode += this.MORSE[d];
+            if (idx < sStr.length - 1) morseCode += ' ';
+        });
+        this.morseS = morseCode;
+    }
+
 
     private blinkHSetter = (v: boolean) => this.blinkH = v;
     private blinkMSetter = (v: boolean) => this.blinkM = v;
-    private blinkSSetter = (v: boolean) => this.blinkS = v;
-
     private timerSetterH = (h: any) => this.timerH = h;
     private timerSetterM = (m: any) => this.timerM = m;
-    private timerSetterS = (s: any) => this.timerS = s;
 
     private buildAndPlay(val: string, ledSetter: (on: boolean) => void, timerSetter: (t: any) => void, T: MorseTiming)
     {
         clearTimeout(timerSetter === this.timerSetterH ?
-            this.timerH : timerSetter === this.timerSetterM ? this.timerM : this.timerS);
+            this.timerH : timerSetter === this.timerSetterM ? this.timerM : null);
 
         let raw = val;
         if (ledSetter === this.blinkHSetter)
@@ -78,7 +85,6 @@ export class MorseClockComponent implements OnDestroy
         }
 
         const digits = raw.padStart(2, '0').split('');
-
         const seq: {on: boolean; dur: number} [] = [];
         digits.forEach((d, idx) =>
         {
@@ -88,15 +94,14 @@ export class MorseClockComponent implements OnDestroy
                 seq.push({on: true, dur: code[i] === '.' ? T.dot : T.dash});
                 if (i < code.length - 1) seq.push({on: false, dur: T.intra});
             }
-            if (idx < digits.length - 1) seq.push({ on: false, dur: T.letter });
+            if (idx < digits.length - 1) seq.push({on: false, dur: T.letter});
         });
         seq.push({on: false, dur: T.loop});
-
         this.zone.runOutsideAngular(() =>
             {timerSetter(setTimeout(() => {this.zone.run(() => this.playStep(seq, 0, ledSetter, timerSetter));}, 50));});
     }
 
-    private playStep(seq: { on: boolean; dur: number }[], idx: number, ledSetter: (b: boolean) => void,
+    private playStep(seq: {on: boolean; dur: number}[], idx: number, ledSetter: (b: boolean) => void,
         timerSetter: (t: any) => void)
     {
         const step = seq[idx];
